ed7f718a38a419e4ddff7177d2c381d68053fd1b
weekMoon <106305324+weekMoon@users.noreply.github.com>
2022-05-27 09:36:36 +0800

    Update GitTest.java
    更新

--- src/main/java/com/ryit/git/GitTest.java
+++ src/main/java/com/ryit/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("更新更新");
     }
 }
