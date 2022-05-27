e0fa0ece55eca9b3cacacc77de45444a81347d95
hi <china_zhou2019@163.com>
2022-05-27 01:28:31 +0000

    update src/main/java/com/ryit/git/GitTest.java. 增加一行数据

--- src/main/java/com/ryit/git/GitTest.java
+++ src/main/java/com/ryit/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test2");
+        System.out.println("IDE 更新");
     }
 }
